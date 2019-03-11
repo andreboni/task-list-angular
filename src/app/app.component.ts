49fd0a4fd3e5b80edf4d4429dd0c09336430c1a4
Felipe de Souza Paiva <spaiva.felipe@gmail.com>
2019-03-11 12:06:49 -0300

    Initial app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { tasks as tasksData } from 'src/model/tasks';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'angular-app-boni';
+  title = "Todo list";
+
+  @ViewChild("inputField") taskLabel;
+  @ViewChild("priorityField") taskPriority;
+
+  priorityOrder = -1;
+  query = "";
+  tasks = [];
+
+  addTask(inputField, $event) {
+    $event.preventDefault();
+    this.tasks.push({
+      label: this.taskLabel.nativeElement.value,
+      priority: parseInt(this.taskPriority.nativeElement.value)
+    });
+    this.resetForm();
+  }
+
+  private resetForm() {
+    this.taskLabel.nativeElement.value = "";
+    this.taskPriority.nativeElement.value = "1";
+  }
+
+  ngDoCheck() {
+    this.getTasks();
+  }
+
+  getTasks() {
+    this.tasks = tasksData
+      .filter(task => {
+        const label = task.label.toLowerCase();
+        const query = this.query.toLowerCase();
+        return label.includes(query);
+      })
+      .sort((a, b) => {
+        const prioritySelected = parseInt(this.priorityOrder);
+        if (a.priority > b.priority) return prioritySelected;
+        if (a.priority < b.priority) return prioritySelected === -1 ? 1 : -1;
+        if (a.priority === b.priority) return 0;
+      });
+  }
 }
---
+++ src/model/tasks.ts
@@ -0,0 +1,42 @@
+export const tasks = [
+  {
+    label: "Comprar pão",
+    priority: 3
+  },
+  {
+    label: "Ir na farmácia",
+    priority: 2
+  },
+  {
+    label: "Pagar os boletos",
+    priority: 2
+  },
+  {
+    label: "Ir na casa do seu Zé",
+    priority: 1
+  },
+  {
+    label: "Carregar o celular",
+    priority: 3
+  },
+  {
+    label: "Limpar a casa",
+    priority: 1
+  },
+  {
+    label: "Comprar teclado novo",
+    priority: 1
+  },
+  {
+    label: "Ir no açougue",
+    priority: 1
+  },
+  {
+    label: "Ir no mercado",
+    priority: 1
+  },
+  {
+    label: "Fazer café",
+    priority: 1
+  }
+];
